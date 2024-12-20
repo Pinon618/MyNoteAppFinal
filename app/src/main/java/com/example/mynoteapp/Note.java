b1f9737eba885a56c22040c77ebdddaa5c84425c
Administrator <minhazu618@gmail.com>
2024-12-20 11:48:07 +0600

    initial commit

---
+++ app/src/main/java/com/example/mynoteapp/Note.java
@@ -0,0 +1,20 @@
+package com.example.mynoteapp;
+
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "notes")
+public class Note {
+    @PrimaryKey(autoGenerate = true)
+    public int id;
+
+    @ColumnInfo(name = "title")
+    public String title;
+
+    @ColumnInfo(name = "content")
+    public String content;
+
+    @ColumnInfo(name = "timestamp")
+    public long timestamp;
+}
---
+++ app/src/main/java/com/example/mynoteapp/NoteDatabase.java
@@ -0,0 +1,27 @@
+package com.example.mynoteapp;
+
+
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+@Database(entities = {Note.class}, version = 1)
+public abstract class NoteDatabase extends RoomDatabase {
+    private static volatile NoteDatabase INSTANCE;
+
+    public abstract NoteDao noteDao();
+
+    public static NoteDatabase getDatabase(final Context context) {
+        if (INSTANCE == null) {
+            synchronized (NoteDatabase.class) {
+                if (INSTANCE == null) {
+                    INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
+                            NoteDatabase.class, "note_database").build();
+                }
+            }
+        }
+        return INSTANCE;
+    }
+}
